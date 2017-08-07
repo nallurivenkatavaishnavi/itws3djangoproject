de8afb8775c13d6caa49ded6bb97c52a41fbb0ec
nallurivenkatavaishnavi <30748976+nallurivenkatavaishnavi@users.noreply.github.com>
2017-08-07 12:03:54 +0530

    Update views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 from django.template import loader
 from django.http import Http404
-
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from .models import Question
 
